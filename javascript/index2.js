8e1085e8aa37f0d26991ed0c8deb3e409599d78f
essoft <essoft@essofts-Mac-mini-4.local>
2020-05-22 09:49:43 +0700

    deploy

---
+++ javascript/index2.js
@@ -0,0 +1,36 @@
+let maps;
+let position = { lat: 13.883932, lng: 100.512292 }
+
+let positionArr = JSON.parse(localStorage.myArrData);
+
+
+function initMap() {
+    maps = new google.maps.Map(document.getElementById('map'), {
+        center: position,
+        zoom: 12
+    });
+
+    let officeMarker = new google.maps.Marker({
+        position: position,
+        map: maps,
+        animation: google.maps.Animation.BOUNCE,
+    })
+
+    let marker, info;
+    let i;
+
+    $.each(positionArr, function (i, item) {
+        marker = new google.maps.Marker({
+            position: new google.maps.LatLng(item.lat, item.lng),
+            map: maps
+        })
+    })
+
+    info = new google.maps.InfoWindow();
+    google.maps.event.addListener(marker, 'click', (function (marker, i) {
+        return function () {
+            info.setContent(item.location);
+            info.open(maps, marker)
+        }
+    })(marker, i));
+}
